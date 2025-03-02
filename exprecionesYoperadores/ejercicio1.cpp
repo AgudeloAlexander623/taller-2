85029d0e8c87f77628454b11a6dc856dd72237c9
Alexander Agudelo <agudeloalexander623@gmail.com>
2025-03-02 13:30:56 -0500

    Proyecto Terminado

--- exprecionesYoperadores/ejercicio1.cpp
+++ exprecionesYoperadores/ejercicio1.cpp
@@ -7,5 +7,11 @@
 using namespace std;
 
 int main(){
-    
+    float a, b, resultado = 0;
+    cout << "Ingrese el valor de a: ";
+    cin >> a;
+    cout << "Ingrese el valor de b: ";
+    cin >> b;
+    resultado = pow((a+b),1);
+    cout << "El resultado de la exprecion es: " << resultado << endl;
 }
